9b6321a64c26a4acff28304eedc6246cf073dda3
aDragon <xdima2003@gmail.com>
2022-10-10 11:58:10 +0300

    Init

--- lr2.c
+++ lr2.c
@@ -3,26 +3,24 @@
 
 float task1(float x, float y)
 {
-    if (x < 0)
-        return 3 * x + 1;
-    else if (x == 0)
-        return y + cos(x);
-    else if (x > 0)
-        return y - sin(x);
+    if (x < 1)
+        return 1;
+    else if (x < 2)
+        return x + 5;
+    else if (x < 4)
+        return 6 * y * 2 * x + 4;
+    else
+        return x + sin(y);
 }
 
 float max(float a, float b)
 {
     if (a > b)
-        return a;
+        return 1;
     else
-        return b;
+        return 0;
 }
 
-float task2(float a, float b, float c)
-{
-    return max(max(a, b), c);
-}
 int main(void)
 {
     // Task1:
@@ -32,12 +30,12 @@ int main(void)
     scanf("%f", &x);
     printf("y = ");
     scanf("%f", &y);
-    printf("F(%g , %g) = %g \n", x, y, task1(-1, 0));
+    printf("F(%g , %g) = %g \n", x, y, task1(x, y));
 
     // Task2:
-    float a, b, c;
-    printf("Введите 3 для задания 2: \n");
-    scanf("%f %f %f", &a, &b, &c);
-    printf(" Максимальное, среди чисел %G,%G,%G - %G", a, b, c, task2(a, b, c));
+    printf("Введите 2 числа для задания 2: \n");
+    scanf("%f %f", &x, &y);
+    max(x, y) == 1 ? printf("%g > %g => выводим %3g", x, y, x) : printf("%g <= %g => выводим %g %g", x, y, x, y);
+
     return 0;
 }
